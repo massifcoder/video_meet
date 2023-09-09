707424f86e0564a2622c9ca87851b04ea99d9b19
Vishal Sharma <massifcoder>
2023-09-09 11:02:11 +0530

    tried dynamic

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import io from 'socket.io-client';
 import SocketContext from './socketContext';
+const baseUrl = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+
+console.log(baseUrl);
 const URL = 'ws://localhost:5000';
 const ios = io(URL);
 
--- server/server.js
+++ server/server.js
@@ -10,11 +10,7 @@ const app = express();
 app.use(cors())
 const server = http.createServer(app);
 const io = socketIO(server, {
-  cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true
-  }
+  cors
 });
 
 const onlineUsers = {};
