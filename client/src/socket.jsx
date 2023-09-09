e85df287e433a08420fea8a8596dbe64c59a9bdb
Vishal Sharma <massifcoder>
2023-09-09 12:07:12 +0530

    updated new url

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import io from 'socket.io-client';
 import SocketContext from './socketContext';
-// const base_url = 
-const URL = 'ws://localhost:5000';
+
+const URL = 'ws://kinjo-meet.onrender.com:10000';
 const ios = io(URL);
 
 const SocketProvider = ({ children }) => {
