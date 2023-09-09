828f0ee8218e400f0f4ce2f3ca0fded7b63b76e1
Vishal Sharma <massifcoder>
2023-09-09 12:41:22 +0530

    added

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -4,6 +4,7 @@ import SocketContext from './socketContext';
 
 const URL = 'wss://kinjo-meet.onrender.com:10000';
 const ios = io(URL);
+console.log('Connection established with status ',ios);
 
 const SocketProvider = ({ children }) => {
   return <SocketContext.Provider value={ios}>{children}</SocketContext.Provider>;
